535f3de7a44561b88363565119684daee53fb8b3
tvhahn <tvhahn@gmail.com>
2020-01-28 15:46:29 -0500

    update

--- split_data.sh
+++ split_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --time=00:10:00 # 10 minutes
 #SBATCH --array=1-3
+#SBATCH --mem=1G
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
 ## How to use arrays: https://docs.computecanada.ca/wiki/Job_arrays
