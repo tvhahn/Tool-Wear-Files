5fc00e897ea930a987e9b06cd7120b312817524f
tvhahn <tvhahn@gmail.com>
2020-01-30 10:03:13 -0500

    requested more memory

--- bash_merge_csv.sh
+++ bash_merge_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=00:10:00 # 10 minutes
-#SBATCH --mem=512M
+#SBATCH --mem=1G
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
 
