45b07227274d666e8d3541934950e336006f6375
tvhahn <tvhahn@gmail.com>
2020-05-04 14:42:05 -0400

    update

--- bash_random_search_single.sh
+++ bash_random_search_single.sh
@@ -12,6 +12,8 @@ module load scipy-stack/2019b
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
+# pip install --no-index scikit_learn
+# pip install --no-index xgboost
 pip install /home/tvhahn/scikit_learn-0.22.1-cp37-cp37m-linux_x86_64.whl
 pip install /home/tvhahn/imbalanced_learn-0.6.1-py3-none-any.whl
 pip install /home/tvhahn/xgboost-0.90-cp37-cp37m-linux_x86_64.whl
