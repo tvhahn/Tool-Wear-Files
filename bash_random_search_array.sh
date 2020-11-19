63e5ffe1ba5b60bbb7567711c6668122a2a82582
tvhahn <tvhahn@gmail.com>
2020-11-19 10:06:32 -0500

    update

--- bash_random_search_array.sh
+++ bash_random_search_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=01:00:00 # 1 hour
-#SBATCH --array=1-200
+#SBATCH --array=1-100
 #SBATCH --mem=2G
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
